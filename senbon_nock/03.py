e3f6d31af077e82fd2a4f51bc96b24a597b9761f
mitsui shouhei <zeze_respoul@mitsuinoMacBook-Air.local>
2019-12-07 23:46:50 +0900

    03.pyの追加

---
+++ senbon_nock/03.py
@@ -0,0 +1,15 @@
+## 03. 円周率
+## "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ
+
+string = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+print("string {}".format(string))
+result = []
+seperate_words = string.split(' ')
+for seperate_word in seperate_words:
+    count = 0
+    for char in seperate_word:
+        if char.isalpha():
+            count += 1
+    result.append(count)
+
+print("result: {}".format(result))
